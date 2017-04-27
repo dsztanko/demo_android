634fa8bcc87d27386c2b564b70c0152f58b3212e
dsztanko <szt.dori@gmail.com>
2017-04-27 16:49:27 +0200

    self notification implemented for demo

--- app/src/main/java/com/codecool/mydemoapp/MainActivity.java
+++ app/src/main/java/com/codecool/mydemoapp/MainActivity.java
@@ -1,52 +1,45 @@
 package com.codecool.mydemoapp;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private static String TAG = ">>>>> " + MainActivity.class.getName();
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    public void onClickNotification(View view) {
+        Log.i(TAG, R.string.notify_me + " clicked");
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        EditText et1 = (EditText) findViewById(R.id.editText1);
+        EditText et2 = (EditText) findViewById(R.id.editText2);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        String title = et1.getText().toString().trim();
+        String body = et2.getText().toString().trim();
 
-        return super.onOptionsItemSelected(item);
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notify = new NotificationCompat.Builder
+                (this)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.notif_icon)
+                .build();
+
+        notify.flags |= Notification.FLAG_AUTO_CANCEL;
+        notificationManager.notify(0, notify);
+        Log.i(TAG, "User has been notified.");
     }
 }
